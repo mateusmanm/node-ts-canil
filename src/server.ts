e3cff1ca0e27a092d3925c99597e633df34621c7
Mateus Martins <mateus.a.n.martins@gmail.com>
2022-05-21 19:12:17 -0300

    configuração inicial de rotas e controllers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -13,7 +15,10 @@ server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 // Rotas
-
+server.use(mainRoutes);
+server.use((req: Request, res: Response) => {
+    res.send('Página não encontrada');
+});
 
 // Pasta Publica
 server.use(express.static(path.join(__dirname, '../public')));
